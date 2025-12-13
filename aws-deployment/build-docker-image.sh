18e96f0c2ed05caa1ed6d4e7c6a8c03e856e7eb4
Ben Taylor <bentaylortheonly@gmail.com>
2025-12-13 05:55:38 +0000

    simplify

--- aws-deployment/build-docker-image.sh
+++ aws-deployment/build-docker-image.sh
@@ -3,14 +3,9 @@ set -e
 
 # Build OSRM Docker image for AWS Batch (AMD64 architecture)
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
-
-cd "${PROJECT_ROOT}"
-
 docker buildx build \
   --platform linux/amd64 \
-  -f Dockerfile.aws-batch \
+  -f ./aws-deployment/Dockerfile.aws-batch \
   -t osrm-aws-batch:latest \
   --load \
   .
