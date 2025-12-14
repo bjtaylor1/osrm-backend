b86c7a72323b9251c6109ce2d4a3625eb791eebb
Ben Taylor <bentaylortheonly@gmail.com>
2025-12-14 21:58:31 +0000

    move lualib files

--- profiles/bicycle_paved.lua
+++ profiles/bicycle_paved.lua
@@ -7,7 +7,7 @@ Sequence = require('lib/sequence')
 Handlers = require("lib/way_handlers")
 find_access_tag = require("lib/access").find_access_tag
 limit = require("lib/maxspeed").limit
-require("lualib/surfacewhitelist")
+require("lib/surfacewhitelist")
 
 function setup()
 
--- profiles/lib/access.lua
+++ profiles/lib/access.lua
@@ -1,15 +1,13 @@
 local ipairs = ipairs
 
-local Access = {}
+module "Access"
 
-function Access.find_access_tag(source,access_tags_hierarchy)
-    for i,v in ipairs(access_tags_hierarchy) do
+function find_access_tag(source,access_tags_hierachy)
+    for i,v in ipairs(access_tags_hierachy) do
         local tag = source:get_value_by_key(v)
-        if tag then
+        if tag ~= '' then
             return tag
         end
     end
     return nil
 end
-
-return Access
