07d8f2b9ca0080554403fe1331071372ddb645d7
Ben Taylor <bentaylortheonly@gmail.com>
2025-12-25 09:51:14 +0000

    update according to simplicity

--- aws-deployment/push-docker-image.sh
+++ aws-deployment/push-docker-image.sh
@@ -4,7 +4,7 @@ set -e
 # Push OSRM Docker image to AWS ECR
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/osrm-process-data"
+ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/osrm-processor"
 
 aws ecr get-login-password --region us-east-1 | \
   docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
