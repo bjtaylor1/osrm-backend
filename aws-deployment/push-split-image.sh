70e48214e86365881b8235b0899010d30e931dd2
Ben Taylor <bentaylortheonly@gmail.com>
2025-12-26 11:32:13 +0000

    fix issues

--- aws-deployment/push-split-image.sh
+++ aws-deployment/push-split-image.sh
@@ -9,5 +9,5 @@ ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/osrm-split"
 aws ecr get-login-password --region us-east-1 | \
   docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
 
-docker tag osrm-process-data:latest "${ECR_REPO}:latest"
+docker tag osrm-split-data:latest "${ECR_REPO}:latest"
 docker push "${ECR_REPO}:latest"
